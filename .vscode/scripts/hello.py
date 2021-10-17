839ab652072ebff44d1e9103a98d64def016112b
rumaan3 <44205152+rumaan3@users.noreply.github.com>
2021-10-17 17:34:00 +0530

    Delete .vscode/scripts directory

--- .vscode/scripts/hello.py
+++
@@ -1,2 +0,0 @@
-msg='heelo world'
-print(msg)
--- .vscode/scripts/selenium.py
+++
@@ -1,6 +0,0 @@
-import selenium
-from selenium import webdriver
-
-driver=webdriver.chrome()
-driver.get('https://www.reddit.com/')
-print('opening browser')
